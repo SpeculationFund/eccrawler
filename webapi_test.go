d928c477cba2fdc87b0edf0855a3b1f4241b570f
phoenix.lv <phoenix.lv@dorry.space>
2017-12-29 19:05:18 +0800

    add exception cases, however without testing them

--- webapi_test.go
+++ webapi_test.go
@@ -7,10 +7,12 @@ import (
     "github.com/nbio/st"
     "gopkg.in/h2non/gock.v1"
     "testing"
+    "encoding/json"
+//    "fmt"
 )
 
 
-func TestGet_WebAPI(t *testing.T) {
+func TestGet_Simple(t *testing.T) {
     defer gock.Off()
 
     gock.New("http://coincheck.com").
@@ -28,3 +30,57 @@ func TestGet_WebAPI(t *testing.T) {
     st.Expect(t, gock.IsDone(), true)
 
 }
+
+
+func TestGet_WebAPI(t *testing.T) {
+    defer gock.Off()
+
+    webapi := &C_WebAPI{}
+    result := make(chan interface{})
+
+    cases := []struct{
+        Host string
+        Url string
+        StatusCode int
+        ResultMsg map[string]string
+    }{
+        {
+            Host: "http://coincheck.com",
+            Url: "/api/ticker",
+            StatusCode: 200,
+            ResultMsg: map[string]string{"foo": "bar"},
+        },
+        /*
+        {
+            Host: "xxx",
+            Url: "/api/ticker",
+            StatusCode: 200,
+            ResultMsg: map[string]string{},
+        },
+        {
+            Host: "http://coincheck.com",
+            Url: "/api/ticker",
+            StatusCode: 200,
+            ResultMsg: nil,
+        },
+        */
+
+
+    }
+
+    for _, c := range cases {
+        gock.New(c.Host).
+            Get(c.Url).
+            Reply(c.StatusCode).
+            JSON(c.ResultMsg)
+        ioresult, _ := json.Marshal(c.ResultMsg)
+        go webapi.Get(c.Host+c.Url, result)
+        st.Expect(t, (<-result).([]byte)[:len(ioresult)], ioresult)
+    }
+
+    st.Expect(t, gock.IsDone(), true)
+
+}
+
+
+
